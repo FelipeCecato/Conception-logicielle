f042c26feab9fd851b20bb5284f9f7c13a1022e9
Lucas <34555408+Luck6o@users.noreply.github.com>
2026-04-22 17:18:37 +0200

    Create PersonneFactory.java

---
+++ TP3/PersonneFactory.java
@@ -0,0 +1,8 @@
+public class PersonneFactory {
+    public static Personne creer(String type, String nom, int age, String id, double moy) {
+        if (type.equalsIgnoreCase("ETUDIANT")) {
+            return new Etudiant(nom, age, id, moy); [cite: 191]
+        }
+        return null;
+    }
+}
