86ea63f7950e84b03d29ea75e3f434d497a0bf2d
Lucas <34555408+Luck6o@users.noreply.github.com>
2026-04-22 17:24:08 +0200

    Update models.py

--- TP3/models.py
+++ TP3/models.py
@@ -16,8 +16,8 @@ class MentionStandard(CalculMention):
 
 class Personne(ABC):
     def __init__(self, nom: str, age: int):
-        self.nom = nom [cite: 185]
-        self.age = age [cite: 185]
+        self.nom = nom 
+        self.age = age 
 
     @abstractmethod
     def afficher_details(self):
@@ -26,21 +26,21 @@ class Personne(ABC):
 class Etudiant(Personne):
     def __init__(self, nom, age, num_etudiant, moyenne):
         super().__init__(nom, age)
-        self.num_etudiant = num_etudiant [cite: 186]
-        self.moyenne = moyenne [cite: 186]
-        self.liste_cours = [] [cite: 186]
+        self.num_etudiant = num_etudiant 
+        self.moyenne = moyenne 
+        self.liste_cours = [] 
         self.observateurs = []
-        self.strategy = MentionStandard() [cite: 197]
+        self.strategy = MentionStandard() 
 
     def ajouter_note(self, nouvelle_note):
         self.moyenne = (self.moyenne + nouvelle_note) / 2
-        self.notifier(f"Note mise a jour pour {self.nom}") [cite: 199]
+        self.notifier(f"Note mise a jour pour {self.nom}") 
 
     def set_strategy(self, strategy: CalculMention):
-        self.strategy = strategy [cite: 198]
+        self.strategy = strategy 
 
     def notifier(self, msg):
-        for obs in self.observateurs: [cite: 199]
+        for obs in self.observateurs: 
             obs.update(msg)
 
     def afficher_details(self):
