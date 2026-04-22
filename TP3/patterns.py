1796dd77a33b6f11bf7122612cb00feb378e21a7
Lucas <34555408+Luck6o@users.noreply.github.com>
2026-04-22 17:24:55 +0200

    Update patterns.py

--- TP3/patterns.py
+++ TP3/patterns.py
@@ -1,34 +1,34 @@
 from .models import Observer, Etudiant, Personne
 
 class ScolariteManager(Observer):
-    _instance = None [cite: 189]
+    _instance = None 
     
     def __new__(cls):
         if cls._instance is None:
             cls._instance = super(ScolariteManager, cls).__new__(cls)
-            cls._instance.etudiants = [] [cite: 189]
+            cls._instance.etudiants = [] 
         return cls._instance
 
     def update(self, message: str):
-        print(f"[OBSERVER] ScolariteManager: {message}") [cite: 199]
+        print(f"[OBSERVER] ScolariteManager: {message}") 
 
 class PersonneFactory:
     @staticmethod
     def creer(type_p, nom, age, **kwargs):
         if type_p.lower() == "etudiant":
-            return Etudiant(nom, age, kwargs.get('id'), kwargs.get('moy')) [cite: 191]
+            return Etudiant(nom, age, kwargs.get('id'), kwargs.get('moy')) 
         return None
 
 class EtudiantBoursier:
     def __init__(self, etudiant: Etudiant):
-        self._etudiant = etudiant [cite: 193]
+        self._etudiant = etudiant 
 
     def afficher_details(self):
-        return self._etudiant.afficher_details() + " [STATUT: BOURSIER]" [cite: 193]
+        return self._etudiant.afficher_details() + " [STATUT: BOURSIER]"
 
 class CourseAdapter:
     @staticmethod
     def transformer(legacy_data: str):
-        # Format "NomCours;Prof" [cite: 194]
-        nom, prof = legacy_data.split(";") [cite: 195]
+        # Format "NomCours;Prof" 
+        nom, prof = legacy_data.split(";") 
         return {"nom": nom, "prof": prof}
